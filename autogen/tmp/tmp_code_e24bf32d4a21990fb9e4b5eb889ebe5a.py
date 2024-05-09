d9501a002782ea0e2898c7d5f2e358f002b7f780
Harry Wang <harryjwang@gmail.com>
2024-05-09 11:02:35 -0400

    ignore tmp

--- autogen/tmp/tmp_code_e24bf32d4a21990fb9e4b5eb889ebe5a.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-x = np.random.randint(0, 100, 100)
-y = np.random.randint(0, 100, 100)
-plt.scatter(x, y)
-plt.savefig('scatter.png')
-print('Scatter plot saved to scatter.png')
